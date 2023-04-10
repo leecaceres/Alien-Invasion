1b2dfbeee838b6e8ac32290054c7773573fcd125
Lee <amecac21@gmail.com>
2023-04-10 00:18:57 -0400

    Using a turtle instead, add left/right movements

--- ship.py
+++ ship.py
@@ -9,12 +9,24 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images/turtle.bmp')
+        self.image = pygame.transform.scale(self.image, (48, 48))
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
-    
+
+        # Movement flags
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement flags."""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+
     def blitme(self):
         """Draw the ship at its current location"""
         self.screen.blit(self.image, self.rect)
