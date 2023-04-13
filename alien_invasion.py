0fece0d75b432a871054ebc28b5c60e4113bbdc0
Lee <amecac21@gmail.com>
2023-04-12 23:36:00 -0400

    aesthetic fixes

--- alien_invasion.py
+++ alien_invasion.py
@@ -252,9 +252,7 @@ class AlienInvasion:
         # Draw the play button if the game is inactive.
         if not self.stats.game_active:
             self.play_button.draw_button()
-        pygame.display.flip()
-
-    
+        pygame.display.flip()   
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
